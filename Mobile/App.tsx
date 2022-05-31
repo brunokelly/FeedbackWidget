13c442c0703e94cb20f8a8e7c3c122ccdde32f88
brunokelly <brunobuch00@gmail.com>
2022-05-30 21:02:33 -0300

    Creating mobile version and adding widget

---
+++ Mobile/App.tsx
@@ -0,0 +1,28 @@
+import AppLoading from "expo-app-loading";
+import { useFonts } from "@expo-google-fonts/inter";
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import SplashScreen from "expo-splash-screen";
+
+import { Widget } from "./src/components/Widget";
+import { theme } from "./src/theme";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
+      <Widget />
+
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+    </View>
+  );
+}
